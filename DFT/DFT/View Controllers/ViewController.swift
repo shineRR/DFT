4dfd8bb1176461221d02e185f59ce00a9dbf132b
Ilya Baryko <barykoilyass@gmail.com>
2021-10-03 17:02:45 +0300

    Init project

---
+++ DFT/DFT/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+// ViewController.swift
+// DFT
+//
+// Created by Ilya Baryko on 3.10.21.
+// 
+//
+
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    // MARK: - Override
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}

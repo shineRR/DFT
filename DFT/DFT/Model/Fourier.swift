eb3032cb202e559c53f1a842d4ad97f5b06845a8
Ilya Baryko <barykoilyass@gmail.com>
2021-10-03 19:22:16 +0300

    DFT and IDFT

---
+++ DFT/DFT/Model/Fourier.swift
@@ -0,0 +1,101 @@
+//
+// Fourier.swift
+// DFT
+//
+// Created by Ilya Baryko on 3.10.21.
+// 
+//
+
+
+import Foundation
+import Numerics
+
+class Fourier {
+    static func getW(for k: Int, with n: Int) -> Complex<Double> {
+        guard k % n != 0 else { return  Complex<Double>(1.0) }
+        let arg = -2.0 * Double.pi * Double(k) / Double(n)
+        return Complex(cos(arg), sin(arg))
+    }
+    
+    static func fft(x: [Complex<Double>]) -> [Complex<Double>] {
+        let xLength = x.count
+        if xLength == 1 { return x }
+        if xLength % 2 != 0 { return x }
+        
+        var newX = [Complex<Double>]()
+        if (xLength == 2) {
+            newX.append(x[0] + x[1])
+            newX.append(x[0] - x[1])
+        } else {
+            let halfLength = xLength / 2
+            
+            var xEven = [Complex<Double>]()
+            var xOdd = [Complex<Double>]()
+            
+            for i in 0..<halfLength {
+                xEven.append(x[2 * i])
+                xOdd.append(x[2 * i + 1])
+            }
+            
+            let xEvenNext = fft(x: xEven)
+            let xOddNext = fft(x: xOdd)
+            
+            newX.removeAll()
+            newX.append(contentsOf: Array.init(repeating: Complex<Double>(1.0), count: xLength))
+            
+            for i in 0..<halfLength {
+                newX[i] = xEvenNext[i] + getW(for: i, with: xLength) * xOddNext[i]
+                newX[i + halfLength] = xEvenNext[i] - getW(for: i, with: xLength) * xOddNext[i]
+            }
+        }
+        
+        return newX
+    }
+    
+    static func dft(in x: [Complex<Double>]) -> [Complex<Double>] {
+        var output = [Complex<Double>]()
+        let length = x.count
+        
+        for i in 0..<length {
+            var re = 0.0
+            var im = 0.0
+            
+            for j in 0..<length {
+                let angle = 2.0 * Double.pi * Double(j) * Double(i) / Double(length)
+                re += x[j].real * cos(angle) - x[j].imaginary * sin(angle)
+                im += -x[j].real * sin(angle) + x[j].imaginary * cos(angle);
+            }
+            output.append(Complex<Double>(re, im))
+        }
+        
+        return output
+    }
+    
+    static func idft(in x: [Complex<Double>]) -> [Complex<Double>] {
+        var output = [Complex<Double>]()
+        let length = x.count
+        
+        for i in 0..<length {
+            var re = 0.0
+            var im = 0.0
+            
+            for j in 0..<length {
+                let angle = 2.0 * Double.pi * Double(j) * Double(i) / Double(length)
+                re += x[j].real * cos(angle) + x[j].imaginary * sin(angle)
+                im += x[j].real * sin(angle) + x[j].imaginary * cos(angle);
+            }
+            output.append(Complex<Double>(re / Double(length), im / Double(length)))
+        }
+        
+        return output
+    }
+    
+    static func halfReverse(in x: [Complex<Double>]) -> [Complex<Double>] {
+        var output = Array.init(repeating: Complex<Double>(1.0), count: x.count)
+        for i in 0..<(x.count / 2) {
+            output[x.length / 2 - 1 - i] = x[i]
+            output[x.length - 1 - i] = x[x.length / 2 + i]
+        }
+        return output
+    }
+}
--- DFT/DFT/View Controllers/ViewController.swift
+++ DFT/DFT/View Controllers/ViewController.swift
@@ -8,12 +8,29 @@
 
 
 import Cocoa
+import Numerics
 
 class ViewController: NSViewController {
 
+    // MARK: - Properties
+    private let N = 32
+    
     // MARK: - Override
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.execute()
+    }
+    
+    // MARK: - Methods
+    private func execute() {
+        var inData = [Complex<Double>]()
+        
+        for i in 0..<N {
+            inData.append(Complex(Math.cos(3 * i * 0.125) + Math.sin(2 * i * 0.125), 0.0))
+        }
+        
+        // MARK: - DFT
+        let outData = Fourier.dft(in: inData)
     }
 }
